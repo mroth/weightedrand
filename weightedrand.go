f598f8635fea339fee47ab6532ce9b58005c3ce7
Matthew Rothenberg <mrothenberg@gmail.com>
2020-06-16 22:13:50 -0400

    implement NewChoice convenience constructor
    This is not in itself a huge convenience currently, however based on a
    toy implementation utilizing the latest go2 generics draft, it will end
    up being more ergonomic to use a constructor in this fashion where the
    type of T can be inferred rather than having to specify the type in a
    Choice(type T) literal.

--- weightedrand.go
+++ weightedrand.go
@@ -15,10 +15,15 @@ import (
 	"sort"
 )
 
-// Choice is a generic wrapper that can be used to add weights for any object
+// Choice is a generic wrapper that can be used to add weights for any item.
 type Choice struct {
 	Item   interface{}
 	Weight uint
+}
+
+// NewChoice creates a new Choice with specified item and weight.
+func NewChoice(item interface{}, weight uint) Choice {
+	return Choice{Item: item, Weight: weight}
 }
 
 // A Chooser caches many possible Choices in a structure designed to improve
@@ -29,7 +34,7 @@ type Chooser struct {
 	max    int
 }
 
-// NewChooser initializes a new Chooser consisting of the possible Choices.
+// NewChooser initializes a new Chooser for picking from the provided Choices.
 func NewChooser(cs ...Choice) Chooser {
 	sort.Slice(cs, func(i, j int) bool {
 		return cs[i].Weight < cs[j].Weight
